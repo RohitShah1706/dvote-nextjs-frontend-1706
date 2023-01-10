32559efc76780cf84414d260d099a86c225644ac
RohitShah1706 <rlshah03@gmail.com>
2023-01-10 23:40:06 +0530

    added react-moralis login button

--- components/navbar/Navbar.jsx
+++ components/navbar/Navbar.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
-import logo from "../../public/assets/logo.png";
-
+import { ConnectButton } from "web3uikit"
 
 const Navbar = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
@@ -19,8 +18,7 @@ const Navbar = () => {
         </div>
       </div>
       <div className="gpt3__navbar-sign">
-        <p>Sign in</p>
-        <button type="button">Sign up</button>
+        <ConnectButton moralisAuth={false}/>  
       </div>
       <div className="gpt3__navbar-menu">
         {toggleMenu
@@ -36,8 +34,7 @@ const Navbar = () => {
             <p><a href="#blog">Library</a></p>
           </div>
           <div className="gpt3__navbar-menu_container-links-sign">
-            <p>Sign in</p>
-            <button type="button">Sign up</button>
+            <ConnectButton moralisAuth={false}/> 
           </div>
         </div>
         )}
--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
 import '../styles/globals.css'
 import "../styles/navbar.css"
 import "../styles/header.css"
-
+import { MoralisProvider } from 'react-moralis';
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <MoralisProvider initializeOnMount={false}>
+      <Component {...pageProps} />
+    </MoralisProvider>
+  )
 }
 
 export default MyApp
